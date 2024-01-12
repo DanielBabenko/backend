b621e60f48b6c8d733d0319df6b1dab08b68b889
DanielBabenko <danielbabenko606@yandex.ru>
2024-01-12 17:49:02 +0300

    Commitus

--- src/components/AuthorDetails.js
+++ src/components/AuthorDetails.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const AuthorDetails = () => {
     return <div className='detailes'>
-        <label className='author name'>Ledentsov Dmitriy Andreevich</label><br/>
-        <label className='author group'>P32081</label><br/>
-        <label className='author variant'>Variant: 48920</label><br/>
+        <label className='author name'>Бабенко Даниил Александрович</label><br/>
+        <label className='author group'>P3234</label><br/>
+        <label className='author variant'>Вариант: 99432</label><br/>
+        <label className='author name'>(пока что просто тест лабы)</label><br/>
     </div>;
 };
 
